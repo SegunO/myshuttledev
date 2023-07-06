bb9efbbc43be4a76ee46900d27408fabcb77876f
Femi <devopsfemi@gmail.com>
2023-07-06 11:27:19 -0500

    yay

---
+++ AKS-Terraform/providers.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id = "2b3a4013-a245-4ca1-b6cd-34b3f400f127"
+  client_id       = "a71d59b4-90f3-4cff-8404-2476b38a26eb"
+  client_secret   = "KdO8Q~.GVOMlZKsnK9DpuMmz5A9TLrb3yuFd5aVf"
+  tenant_id       = "5fcf9381-1c21-4c91-a50e-1bb0ab3be1aa"
+
+features {}
+}
