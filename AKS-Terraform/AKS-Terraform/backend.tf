aefa79d8626909ae26da1f32da17b4590dad2ae6
Femi Ogunsakin <peace4eva74@gmail.com>
2023-07-06 16:26:20 +0000

    Deleted AKS-Terraform

--- AKS-Terraform/AKS-Terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "femiterrastorage-rg"
-    storage_account_name = "femiterrastorage23"
-    container_name       = "terracontainernow"
-    key                  = "terraform.tfstate"
-  }
-}
--- AKS-Terraform/AKS-Terraform/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-resource_group_name = "aks_tf_rg"
-location            = "EastUS"
-cluster_name        = "my-aks-cluster-femi23"
-kubernetes_version  = "1.26.0"
-system_node_count   = 2
-acr_name            = "femiacr2314"
